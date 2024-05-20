0a57ed12dbbbee9b885185b6a61f679e2f69f0c5
ega-forever <zyev.egor@gmail.com>
2024-05-20 22:31:52 +0300

    *working

--- extention/background.js
+++ extention/background.js
@@ -11,8 +11,8 @@ const getBlobUrl = async (blob) => {
   try {
     await chrome.offscreen.createDocument({
       url,
-      reasons: ['BLOBS'],
-      justification: 'MV3 requirement',
+      reasons: [ 'BLOBS' ],
+      justification: 'MV3 requirement'
     });
   } catch (err) {
     if (!err.message.startsWith('Only a single offscreen')) throw err;
@@ -20,38 +20,20 @@ const getBlobUrl = async (blob) => {
   const client = (await clients.matchAll({includeUncontrolled: true}))
     .find(c => c.url === url);
   const mc = new MessageChannel();
-  client.postMessage(blob, [mc.port2]);
+  client.postMessage(blob, [ mc.port2 ]);
   const res = await new Promise(cb => (mc.port1.onmessage = cb));
   return res.data;
 }
 
+chrome.webNavigation.onCompleted.addListener(async () => {
+
+});
+
 chrome.action.onClicked.addListener(async (tab) => {
-  /*if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState
-    });
-
-    if (nextState === 'ON') {
-      // Insert the CSS file when the user turns the extension on
-      await chrome.scripting.insertCSS({
-        files: ['focus-mode.css'],
-        target: { tabId: tab.id }
-      });
-    } else if (nextState === 'OFF') {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ['focus-mode.css'],
-        target: { tabId: tab.id }
-      });
-    }
-  }*/
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: 'ON'
+  });
 
   //current uri and masterPlaylists (last seen via requests)
   let uri;
@@ -71,6 +53,11 @@ chrome.action.onClicked.addListener(async (tab) => {
       await sem.release();
       return
     }
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: 'DWN'
+    });
 
     const masterPlayListMetaData = m3u8Parser(masterPlayListLocalData, masterPlayListLocalUrl);
     const maxLevel = masterPlayListMetaData.levels.sort((a, b) => b.bandwidth - a.bandwidth)[0];
@@ -123,6 +110,10 @@ chrome.action.onClicked.addListener(async (tab) => {
 
     processedMasterPlaylists.add(masterPlayList);
     await sem.release();
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: 'ON'
+    });
   }
 
   chrome.webRequest.onBeforeRequest.addListener(
